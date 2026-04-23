35d269a8d7da8da23a5b07df41de5a6f0078f246
Vivek Singh Rajawat <vivekrajawat2005@gmail.com>
2026-04-23 09:54:02 +0530

    url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true
 });
+
 
 // REGISTER
 export async function register({ username, email, password }) {
