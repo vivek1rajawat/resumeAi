c4ecd0bbe1c3928f1bd2a06cc7b63157bcea635e
Vivek Singh Rajawat <vivekrajawat2005@gmail.com>
2026-04-23 18:25:14 +0530

    s

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,6 @@ const connectToDB = require("./src/config/database");
 
 connectToDB();
 
-// ✅ Render uses a dynamic port via process.env.PORT
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
