9a90e6b859f536886182befb7d0b477ad30e6a36
Vivek Singh Rajawat <vivekrajawat2005@gmail.com>
2026-04-23 18:30:13 +0530

    re

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ connectToDB();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+// ✅ Render needs binding on 0.0.0.0
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server is running on port", PORT);
 });
