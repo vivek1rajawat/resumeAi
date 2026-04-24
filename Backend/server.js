765f30f7afea6a4a59f9437aa4e024f82856a7b7
Vivek Singh Rajawat <vivekrajawat2005@gmail.com>
2026-04-24 09:14:03 +0530

    node

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,8 @@ const connectToDB = require("./src/config/database");
 
 connectToDB();
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server is running on port", PORT);
 });
